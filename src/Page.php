8d35dbcc1ffbc81b4a0e450efc86b466e84fcb1c
0x6368656174 <0x6368656174@gmail.com>
2018-10-23 15:20:39 +1000

    new page

--- src/Page.php
+++ src/Page.php
@@ -10,31 +10,120 @@ declare(strict_types=1);
 
 namespace ItQuasar\WpHelpers;
 
+use Exception;
 use ReflectionClass;
 use Timber\Timber;
+use function strtolower;
 
-class Page
+/**
+ * Страница.
+ */
+abstract class Page
 {
+  /** @var Page */
+  private static $notFoundPage;
+
+  /**
+   * Устанавливает страницу для 404 (не найдена) ошибку.
+   *
+   * Данная страница будет отображаться в случае вызова @see static::throwNotFoundException().
+   *
+   * @param Page $notFoundPage
+   */
+  public static function setNotFoundPage(Page $notFoundPage): void
+  {
+    static::$notFoundPage = $notFoundPage;
+  }
+
+  /**
+   * Возвращает сгенерированую страницу.
+   *
+   * @return string
+   *
+   * @throws Exception
+   */
   public static function render(): string
   {
-    $context = static::getContext();
-
     $file = static::getTemplateName();
+
+    $context = static::getRenderContext();
+
+    // jsData превратим в json
+    $context['jsData'] = json_encode($context['jsData']);
 
     return Timber::render($file, $context);
   }
 
-  protected static function getContext(): array
+  /**
+   * Возвращает признак того, что текущая страница активна.
+   *
+   * @return bool
+   */
+  abstract public static function isActive(): bool;
+
+  /**
+   * Возвращает 404 ошибку (Страница не найдена).
+   *
+   * Вернет страницу со статусом 404, а так же содержимым, которое возвращает страница,
+   * установленная через функцию @see static::setNotFoundPage().
+   */
+  public static function throwNotFoundException(): void
   {
-    return Timber::get_context();
+    global $wp_query;
+    $wp_query->set_404();
+    status_header(404);
+    nocache_headers();
+    if (static::$notFoundPage) {
+      try {
+        static::$notFoundPage->render();
+      } catch (Exception $e) {
+      }
+    }
+    die();
   }
 
+  /**
+   * Возвращает контекст, который будет использоваться для генерации страницы.
+   *
+   * @return array
+   */
+  protected static function getRenderContext(): array
+  {
+    $context = static::getContext();
+    $extraJsData = [
+      'ajaxUrl' => admin_url('admin-ajax.php'),
+    ];
+    if (isset($context['jsData'])) {
+      $context['jsData'] = array_merge($context['jsData'], $extraJsData);
+    } else {
+      $context['jsData'] = $extraJsData;
+    }
+
+    return array_merge_recursive(Timber::get_context(), $context);
+  }
+
+  /**
+   * Вовзращает контекст
+   *
+   * @return array
+   */
+  abstract protected static function getContext(): array;
+
+  /**
+   * Возвращает имя шаблона, который должен быть использован для генерации страницы.
+   *
+   * @return string
+   *
+   * @throws \ReflectionException
+   */
   protected static function getTemplateName(): string
   {
     $rc = new ReflectionClass(static::class);
     $file = basename($rc->getFileName(), '.php');
-    $file = strtolower($file);
+    // Взято с https://stackoverflow.com/a/19533226/1778685
+    $file = strtolower(preg_replace('/(?<!^)[A-Z]/', '-$0', $file));
 
     return "pages/p-$file/p-$file.twig";
   }
 }
+
